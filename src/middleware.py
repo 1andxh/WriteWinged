fa531bdd8dc77269f19d48d9145a2e01d68969b0
andoh <solomonkingsman@gmail.com>
2026-02-22 10:13:28 +0000

    Add custom logging

--- src/middleware.py
+++ src/middleware.py
@@ -1,11 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from .config import config
+import logging
 
 secret_key = config.MIDDLEWARE_SECRET
 
+logger = logging.getLogger("uvicorn.access")
+
+logger.disabled = True
+
 
 def register_middleware(app: FastAPI):
+    app.add_middleware(
+        TrustedHostMiddleware,
+        www_redirect=True,
+        allowed_hosts=["localhost", "127.0.0.1", "127.0.0.1:6379"],
+    )
 
     app.add_middleware(SessionMiddleware, secret_key)  # required for OAuth to work
+
+    @app.middleware("http")
+    async def get_request_info(request: Request, call_next):
+        print(f"INCOMING: {request.method} {request.url.path}")
+
+        response = await call_next(request)
+
+        print(f"RESPONSE: STATUS {response.status_code}")
+        return response
